c4d338a398c3be4d3c4a427c9a2ed4e9c7c75936
brockcp <brockpatterson@gmail.com>
2023-09-28 11:52:35 -0700

    ...

--- src/layout/Head.js
+++ src/layout/Head.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import './layout.css';
 import logo from './img-tebo-logo2.png';
-import photo from './img-susan-tebo.jpg';
 
 function Head() {
   return (
     <div class="container-fluid header">
        <header>
          <img src={logo} className="logo" alt="logo" />
-         <div className="title">
-           <img src={photo} className="photo" alt="profile" />
-           <h1>SusanTebo</h1>
-           <h2>President</h2>
-         </div>
+
      </header>
    </div>
 
